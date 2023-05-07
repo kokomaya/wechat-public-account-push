c30b8fe7abeff72592147e0764027f256c23429f
PEITIE <38802203+kokomaya@users.noreply.github.com>
2023-05-07 20:06:01 +0800

    update pushpuls token

--- config/index.cjs
+++ config/index.cjs
@@ -26,7 +26,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oop3m5qumUga_Ww34X4Bn_xDtGzc',
+      id: '3bdeb1b95ebe4b159142abd38e8b5b4a',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'smaojEf8aNHug_yyhwhCu5NL0NNV2mkhEha9gUT9ibE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
